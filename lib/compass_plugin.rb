2ef5cfdebfc626a35447cfcf89550e5315188545
Andrey <frolin@undev.ru>
2015-07-11 04:29:11 -0500

    Initial commit

---
+++ lib/compass_plugin.rb
@@ -0,0 +1,27 @@
+# Enables support for Compass, a stylesheet authoring framework based on SASS.
+# See http://compass-style.org/ for more details.
+# Store Compass/SASS files (by default) within 'app/assets'.
+
+module CompassInitializer
+  def self.registered(app)
+    require 'sass/plugin/rack'
+
+    Compass.configuration do |config|
+      config.project_path = Padrino.root
+      config.sass_dir = "app/assets"
+      config.project_type = :stand_alone
+      config.http_path = "/"
+      config.css_dir = "public/stylesheets"
+      config.images_dir = "public/images"
+      config.javascripts_dir = "public/javascripts"
+      config.fonts_dir = "public/fonts"
+      config.output_style =  :expanded
+
+    end
+
+    Compass.configure_sass_plugin!
+    Compass.handle_configuration_change!
+
+    app.use Sass::Plugin::Rack
+  end
+end
