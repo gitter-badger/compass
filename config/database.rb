1249f04b940b866e3cebddf8d03ee923431d3ecf
Andrey Frolov <fralunia@gmail.com>
2015-08-05 16:12:06 +0300

    db_11

--- config/database.rb
+++ config/database.rb
@@ -23,7 +23,18 @@ ActiveRecord::Base.configurations[:development] = {
 }
 
 
-ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+  )
+end
 
 
 # Setup our logger
