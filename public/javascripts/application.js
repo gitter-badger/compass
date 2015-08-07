eaf5385c750c9c7ec32e48dc360955bfae9f74f7
Andrey <frolin@undev.ru>
2015-08-07 18:24:57 -0500

    style ios fix scroll

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,6 @@
 
 
 $(".tab").click(function() {
-    $(".tab").removeClass("active").eq($(this).index()).addClass("active");
+    $(".tab").removeClass("active").eq($(this).index()).addClass("active").css("z-index:-1;");
     $(".tab_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
