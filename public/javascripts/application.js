e38e85193d84f51cfba7cc9bc4abb2643edd92b4
Andrey <frolin@undev.ru>
2015-08-07 15:57:21 -0500

    style fixes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,9 +5,3 @@ $(".tab").click(function() {
     $(".tab").removeClass("active").eq($(this).index()).addClass("active");
     $(".tab_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
-
-$("nav a").click(function(){
-    // изменяем класс самой ссылки
-    $(this).addClass("active");
-    // и ничего не делаем дальше (дабы не было перехода по ссылки)
-});
