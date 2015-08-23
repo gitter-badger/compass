4c046eb7347a1537b124d3a5a1ea0b70d9542abd
Andrey Frolov <fralunia@gmail.com>
2015-08-23 09:07:26 +0300

    optimize step 1

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -20,56 +20,56 @@ $(".tab").click(function() {
 //});
 
 //
-$(document).ready(function() {
-    //window.wiselinks = new Wiselinks($('main'));
+//$(document).ready(function() {
+//    window.wiselinks = new Wiselinks($('main'));
 
-//    var contacts_page = $(".page.contacts");
-//
-//    ymaps.ready(function() {
-//
-//        var $map =  $('#map');
-//        console.log("map loading");
-//
-//        if (contacts_page) {
-//            $map.fadeIn();
-//            console.log("map show");
-//
-//        }
-//        else
-//            {
-//                $map.fadeOut();
-//                console.log("map hide");
-//
-//        }
-//
-//
-//    });
-//
-//    $(document).off('page:loading').on('page:loading', function(event, $target, render, url) {
-//        return console.log("Loading: " + url + " to " + $target.selector + " within '" + render + "'");
-//    });
-//    $(document).off('page:redirected').on('page:redirected', function(event, $target, render, url) {
-//
-//        return console.log("Redirected to: " + url);
-//
-//    });
-//    $(document).off('page:always').on('page:always', function(event, xhr, settings) {
-//        return console.log("Wiselinks page loading completed");
-//    });
-//    $(document).off('page:done').on('page:done', function(event, $target, status, url, data) {
-//        $('main').fadeTo(200, 1, ease);
-//        return console.log("Wiselinks status: '" + status + "'");
-//    });
-//    return $(document).off('page:fail').on('page:fail', function(event, $target, status, url, error, code) {
-//        return console.log("Wiselinks status: '" + status + "'");
-//    });
-
-
-    // for Yandex metrics
+    //var contacts_page = $(".page.contacts");
     //
+    //ymaps.ready(function() {
+    //
+    //    var $map =  $('#map');
+    //    console.log("map loading");
+    //
+    //    if (contacts_page) {
+    //        $map.fadeIn();
+    //        console.log("map show");
+    //
+    //    }
+    //    else
+    //        {
+    //            $map.fadeOut();
+    //            console.log("map hide");
+    //
+    //    }
+    //
+    //
+    //});
+
+    //$(document).off('page:loading').on('page:loading', function(event, $target, render, url) {
+    //    return console.log("Loading: " + url + " to " + $target.selector + " within '" + render + "'");
+    //});
+    //$(document).off('page:redirected').on('page:redirected', function(event, $target, render, url) {
+    //
+    //    return console.log("Redirected to: " + url);
+    //
+    //});
+    //$(document).off('page:always').on('page:always', function(event, xhr, settings) {
+    //    return console.log("Wiselinks page loading completed");
+    //});
+    //$(document).off('page:done').on('page:done', function(event, $target, status, url, data) {
+    //    $('main').fadeTo(200, 1);
+    //    return console.log("Wiselinks status: '" + status + "'");
+    //});
+    //return $(document).off('page:fail').on('page:fail', function(event, $target, status, url, error, code) {
+    //    return console.log("Wiselinks status: '" + status + "'");
+    //});
+
+
+     //for Yandex metrics & Google analitics
+
     //$(document).off('page:done').on('page:done', function(event, $target, status, url, data) {
     //    _gaq.push(['_trackPageview', url]);
     //    return _metrika.hit(url);
     //});
 
-});
+//});
