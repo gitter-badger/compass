a1be8fd0c1e7a1fec50a6c204a80273f1d524c7f
Andrey Frolov <fralunia@gmail.com>
2015-09-07 14:19:34 +0300

    fix bugs

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,6 @@
 // Put your application scripts here
 
 
-$(".tab").click(function() {
-    $(".tab").removeClass("active").eq($(this).index()).addClass("active").css("z-index:-1;");
-    $(".tab_item").hide().eq($(this).index()).fadeIn()
-}).eq(0).addClass("active");
 
 //
 $(document).ready(function() {
@@ -12,26 +8,38 @@ $(document).ready(function() {
 
     $(document).off('page:loading').on('page:loading', function(event, $target, render, url) {
 
+        console.log("Hash change + " + window.location.pathname);
+        console.log("to url " + url);
+
+        if (window.location.pathname == '/price')
+            $('main').addClass('price').removeClass('contacts');
+
+        //.removeClass('pt-page-moveToLeft').addClass('pt-page-moveToLeft pt-page-moveFromLeft');
+        else
+            $('main').addClass('contacts').removeClass('price');
 
 
         //$target.animate({
         //transform: 'translateY(600px)'}, 1000);
         //$('main').show("slide", { direction: "left" }, 500);
+
+        $('main').css({'opacity':0}).animate({'opacity': '1'},500);
+
+
         return console.log("Loading: " + url + " to " + $target.selector + " within '" + render + "'");
     });
     $(document).off('page:redirected').on('page:redirected', function(event, $target, render, url) {
-
-
         return console.log("Redirected to: " + url);
     });
+
     $(document).off('page:always').on('page:always', function(event, xhr, settings) {
 
-        if (window.location.pathname == '/')
-            $('main').addClass('contacts').removeClass('price');
-        //.removeClass('pt-page-moveToLeft').addClass('pt-page-moveToLeft pt-page-moveFromLeft');
-        else
-            $('main').addClass('price').removeClass('contacts')
         //.removeClass('pt-page-moveToLeft pt-page-moveFromLeft').addClass('pt-page-moveToLeft');
+
+        $(".tab").click(function() {
+            $(".tab").removeClass("active").eq($(this).index()).addClass("active").css("z-index:-1;");
+            $(".tab_item").hide().eq($(this).index()).fadeIn()
+        }).eq(0).addClass("active");
 
 
         $('main').css({'opacity':0}).animate({'opacity': '1'},500);
---
+++ public/javascripts/tabs.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+        $(".tab").click(function () {
+            $(".tab").removeClass("active").eq($(this).index()).addClass("active").css("z-index:-1;");
+            $(".tab_item").hide().eq($(this).index()).fadeIn()
+        }).eq(0).addClass("active");
+
+});
