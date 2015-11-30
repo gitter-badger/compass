ac9ef0241699d0f245b285f3046c4799b76e8794
frolin <fralunia@gmail.com>
2015-11-30 15:47:27 +0300

    add vk logo

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -7,8 +7,10 @@ module Compass
 
       def data_csv
         files_path.inject({}) { |hash, file_path|
-        hash.merge( File.basename(file_path, '.csv') => CSV.read(file_path, {headers:true, return_headers: false}) )
-        }
+        hash.merge( File.basename(file_path, '.csv') => CSV.read(file_path, {
+                                                                              headers:true,
+                                                                              return_headers: false,
+                                                                              row_sep: "\n" }) ) }
       end
 
       private
